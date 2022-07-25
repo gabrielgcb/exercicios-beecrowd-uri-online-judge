8dc13214c7273d6faa76c9592bf3903854749a6e
Gabriel <109290690+g-B1sp0@users.noreply.github.com>
2022-07-25 17:14:50 -0300

    Área do Círculo

--- beecrowd 1002.py
+++ beecrowd 1002.py
@@ -5,8 +5,9 @@ Considerando para este problema que pi = 3.14159:
   - Efetue o cálculo da área, elevando o valor do raio ao quadrado e multiplicando por pi.
 """
 
-raio = input()
-raio = float(raio)
+raio = float(input())
 
-Area = 3.14159 * raio**2
+pi = 3.14159
+Area = pi * raio**2
+
 print("A = {:.4f}" .format(Area))
