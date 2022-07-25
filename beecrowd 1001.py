d86b9b57e45b242637241aa2a52999aa33b8a006
Gabriel <109290690+g-B1sp0@users.noreply.github.com>
2022-07-25 16:54:48 -0300

    Create beecrowd 1001.py

---
+++ beecrowd 1001.py
@@ -0,0 +1,11 @@
+"""
+Leia 2 valores inteiros e armazene-os nas variáveis A e B. 
+Efetue a soma de A e B atribuindo o seu resultado na variável X. 
+Imprima X conforme exemplo apresentado abaixo.
+"""
+
+A = input()
+B = input()
+X = int(A) + int(B)
+
+print("X =", X)
