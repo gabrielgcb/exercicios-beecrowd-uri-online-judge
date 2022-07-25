edf786d7da3b9dbff8f5cc74cdff3315d77e3920
Gabriel <109290690+g-B1sp0@users.noreply.github.com>
2022-07-25 17:08:19 -0300

    Produto simples

---
+++ beecrowd 1004.py
@@ -0,0 +1,11 @@
+"""
+Leia dois valores inteiros. 
+A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. 
+A seguir mostre a variável PROD com mensagem correspondente.  
+"""
+
+a = input()
+b = input()
+prod = int(a) * int(b)
+
+print("PROD =", prod)
