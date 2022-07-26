dca8227020f3ef14d346fef9494766d4c301b98b
Gabriel <109290690+g-B1sp0@users.noreply.github.com>
2022-07-26 10:58:31 -0300

    Diferença

--- python/beecrowd 1007.py
+++ python/beecrowd 1007.py
@@ -1,7 +1,7 @@
 """
 Leia quatro valores inteiros A, B, C e D. 
 A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: 
-DIFERENCA = (A * B - C * D).
+DIFERENCA = (A * B - C * D)
 """
 
 vA = int(input())
