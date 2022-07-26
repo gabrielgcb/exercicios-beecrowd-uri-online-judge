9552b4c44a2cea805e81c0e0224206b21c7e5140
Gabriel <109290690+g-B1sp0@users.noreply.github.com>
2022-07-26 11:09:01 -0300

    Distância entre dois pontos

--- python/beecrowd 1015.py
+++ python/beecrowd 1015.py
@@ -1,7 +1,7 @@
 """
-Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, 
-p1(x1,y1) e p2(x2,y2) e 
-calcule a distância entre eles, mostrando 4 casas decimais após a vírgula, segundo a fórmula:
+Leia quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, 
+p1(x1,y1) e p2(x2,y2) e calcule a distância entre eles, 
+mostrando 4 casas decimais após a vírgula, segundo a fórmula:
 
 Distancia = raizQuadrada( (x2 - x1)^2 + (y2 - y1)^2 )
 """
