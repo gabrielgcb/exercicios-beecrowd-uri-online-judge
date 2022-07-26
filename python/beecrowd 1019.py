387b7bfa00da8805a8fe7221336f0492fc6773bc
Gabriel <109290690+g-B1sp0@users.noreply.github.com>
2022-07-26 11:12:20 -0300

    Conversão de tempo

--- python/beecrowd 1019.py
+++ python/beecrowd 1019.py
@@ -1,6 +1,6 @@
 """
 Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, 
-e informe-o expresso no formato horas:minutos:segundos.
+e informe-o no formato horas:minutos:segundos.
 """
 
 tempo_segundos = input()
