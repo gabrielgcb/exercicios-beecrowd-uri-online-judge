d3b64a91981e6d09c017308c7fb09131f77975d9
Gabriel <109290690+g-B1sp0@users.noreply.github.com>
2022-07-26 11:17:58 -0300

    Delete beecrowd 1014.py

--- beecrowd 1014.py
+++
@@ -1,9 +0,0 @@
-# Calcule o consumo médio de um automóvel sendo fornecidos a 
-# distância total percorrida (em Km) e o total de combustível gasto (em litros).
-
-distancia_total = int(input())
-combustivel_gasto = float(input())
-
-consumo = distancia_total / combustivel_gasto
-
-print("{:.3f} km/l".format(consumo))
