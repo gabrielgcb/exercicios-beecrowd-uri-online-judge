9a29a0f860be6966516b8a79c5cc31ecdbd90a1c
Gabriel <109290690+g-B1sp0@users.noreply.github.com>
2022-07-26 10:53:38 -0300

    Extremamente Básico

--- python/beecrowd 1001.py
+++ python/beecrowd 1001.py
@@ -1,7 +1,7 @@
 """
 Leia 2 valores inteiros e armazene-os nas variáveis A e B. 
 Efetue a soma de A e B atribuindo o seu resultado na variável X. 
-Imprima X conforme exemplo apresentado abaixo.
+Imprima X conforme o exemplo apresentado abaixo.
 """
 
 A = input()
