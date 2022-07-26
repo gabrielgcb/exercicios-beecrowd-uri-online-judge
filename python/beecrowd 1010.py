10537b0d888442a8615dff56c039c4a169b5905c
Gabriel <109290690+g-B1sp0@users.noreply.github.com>
2022-07-26 11:00:40 -0300

    Cálculo Simples

--- python/beecrowd 1010.py
+++ python/beecrowd 1010.py
@@ -1,7 +1,7 @@
 """
 Neste problema, deve-se ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, 
 o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. 
-Após, calcule e mostre o valor a ser pago.
+Calcule e mostre o valor a ser pago.
 """
 
 codigo_peca1, numero_pecas1, valor_unid1 = input().split()
