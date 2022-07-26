9374ebcb492ff0146197e76e1fdf5e7786a832ee
Gabriel <109290690+g-B1sp0@users.noreply.github.com>
2022-07-26 10:54:26 -0300

    Área do círculo

--- python/beecrowd 1002.py
+++ python/beecrowd 1002.py
@@ -8,6 +8,6 @@ Considerando para este problema que pi = 3.14159:
 raio = float(input())
 
 pi = 3.14159
-Area = pi * raio**2
+area = pi * raio**2
 
-print("A = {:.4f}" .format(Area))
+print("A = {:.4f}" .format(area))
