234187cf3c13345695982b4b72b012997fb299d3
Gabriel Bispo <109290690+gb1sp0@users.noreply.github.com>
2022-07-27 11:42:02 -0300

    Sort Simples

--- java/beecrowd 1042.java
+++ java/beecrowd 1042.java
@@ -1,6 +1,6 @@
 /*
 Leia 3 valores inteiros e ordene-os em ordem crescente. 
-No final, mostre os valores em ordem crescente, uma linha em branco e em seguida, 
+No final, mostre os valores em ordem crescente, uma linha em branco e, em seguida, 
 os valores na sequência como foram lidos.
 */
 
