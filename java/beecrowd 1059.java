11a993dbfed709b95d5ce78eb63e033df61bd41f
Gabriel Bispo <109290690+gb1sp0@users.noreply.github.com>
2022-07-27 11:47:35 -0300

    Números Pares

---
+++ java/beecrowd 1059.java
@@ -0,0 +1,17 @@
+/*
+Faça um programa que mostre os números pares entre 1 e 100, inclusive.
+*/
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) { 
+		
+		int i = 0;
+		
+		while (i < 100) {
+			System.out.println(i += 2);
+		}
+		
+	}
+}
