36c640765a9bbc9967846f8f837817041d878190
Gabriel Bispo <109290690+gb1sp0@users.noreply.github.com>
2022-07-27 11:56:06 -0300

    Menor e Posição

---
+++ java/beecrowd 1180.java
@@ -0,0 +1,55 @@
+/*
+Faça um programa que leia um valor N. Este N será o tamanho de um vetor X[N]. 
+A seguir, leia cada um dos valores de X, encontre o menor elemento deste vetor e 
+a sua posição dentro do vetor, mostrando esta informação.
+
+Entrada
+A primeira linha de entrada contem um único inteiro N (1 < N < 1000), 
+indicando o número de elementos que deverão ser lidos em seguida para o vetor X[N] de inteiros. 
+A segunda linha contém cada um dos N valores, separados por um espaço. 
+Vale lembrar que nenhuma entrada haverá números repetidos.
+
+Saída
+A primeira linha apresenta a mensagem “Menor valor:” seguida de um espaço e do menor valor lido na entrada. 
+A segunda linha apresenta a mensagem “Posicao:” seguido de um espaço e da posição do vetor na qual se encontra o menor valor lido, 
+lembrando que o vetor inicia na posição zero.
+*/
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int tam = scan.nextInt();
+        int vetor[] = new int[tam];
+        int i = 0;
+        
+        while (i < tam) {
+            vetor[i] = scan.nextInt();
+            i++;
+        }
+        
+        int menor = 0;
+        int posicao = 0;
+        int j = 0;
+        
+        while (j < tam) {
+            if (j == 0) {
+                menor = vetor[0];
+            }
+            else {
+                if (vetor[j] < menor) {
+                    menor = vetor[j];
+                    posicao = j;
+                }
+            }
+            j++;
+        }
+        
+        System.out.println("Menor valor: " + menor);
+        System.out.println("Posicao: " + posicao);
+        
+    }
+}
